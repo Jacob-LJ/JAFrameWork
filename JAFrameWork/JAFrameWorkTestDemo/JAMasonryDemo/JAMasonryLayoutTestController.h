f437c5ef436feb620f41ecda34b19a89f0509a0a
Jacob <312846421@qq.com>
2017-10-07 12:11:27 +0800

    some demo code start

---
+++ JAFrameWork/JAFrameWorkTestDemo/JAMasonryDemo/JAMasonryLayoutTestController.h
@@ -0,0 +1,13 @@
+//
+//  JAMasonryLayoutTestController.h
+//  JAFrameWork
+//
+//  Created by Jacob_Liang on 2017/10/7.
+//  Copyright © 2017年 Jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAMasonryLayoutTestController : UIViewController
+
+@end
---
+++ JAFrameWork/JAFrameWorkTestDemo/JAMasonryDemo/JAMasonryLayoutTestController.m
@@ -0,0 +1,79 @@
+//
+//  JAMasonryLayoutTestController.m
+//  JAFrameWork
+//
+//  Created by Jacob_Liang on 2017/10/7.
+//  Copyright © 2017年 Jacob. All rights reserved.
+//
+
+#import "JAMasonryLayoutTestController.h"
+//vc
+#import "DefaultPlaceholderVController.h"
+
+static NSString * const JAMasonryLayoutTestCellID = @"JAMasonryLayoutTestCell";
+
+@interface JAMasonryLayoutTestController ()<UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray *dataArray;
+
+@end
+
+@implementation JAMasonryLayoutTestController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.tableFooterView = [[UIView alloc] init];
+    [self.view addSubview:tableView];
+    
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:JAMasonryLayoutTestCellID];
+    
+}
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dataArray.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:JAMasonryLayoutTestCellID forIndexPath:indexPath];
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    switch (indexPath.row) {
+        case 0:
+            [self.navigationController pushViewController:[[DefaultPlaceholderVController alloc] init] animated:YES];
+            break;
+        case 1:
+            [self.navigationController pushViewController:[[UIViewController alloc] init] animated:YES];
+            break;
+        case 2:
+            [self.navigationController pushViewController:[[UIViewController alloc] init] animated:YES];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+#pragma mark - getter & setter
+- (NSArray *)dataArray {
+    if (!_dataArray) {
+        _dataArray = @[
+                       @"DefaultPlaceholder",
+                       @"CustomPlaceholder",
+                       @"MultiGroupTableView",
+                       ];
+    }
+    return _dataArray;
+}
+
+@end
